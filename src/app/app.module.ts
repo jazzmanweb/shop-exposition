22021cb63e90df414eef7a35df7c9a70c883c186
Maciej Żółtowski <maciej.zoltowski@e-qsg.com>
2019-02-13 21:30:16 +0100

    carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './common/menu/menu.component';
+import { AngularMaterialModule } from './common/angular-material.module';
+import { AppRoutingModule } from './app-routing.module';
+import { ExpositionModule } from './modules/exposition/exposition.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularMaterialModule,
+    ExpositionModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/exposition/exposition.module.ts
+++ src/app/modules/exposition/exposition.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AngularMaterialModule } from 'src/app/common/modules/angular-material.module';
 import { ExpositionRoutingModule } from './exposition-routing.module';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { DetailsComponent } from './components/details/details.component';
 import { ItemComponent } from './components/item/item.component';
 import { TilesComponent } from './components/tiles/tiles.component';
+import { AngularMaterialModule } from 'src/app/common/angular-material.module';
 
 @NgModule({
     declarations: [
